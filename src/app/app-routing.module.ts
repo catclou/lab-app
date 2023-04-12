984686707feb7496b7e3a0e0990ff41c3a18aa96
Caterina Moccia <caterina.moccia@gmail.com>
2023-04-12 10:40:24 +0200

    routing home + login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'features/home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }, { path: 'features/login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
